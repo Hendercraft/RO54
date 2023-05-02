b99eb918db0d21b72a3ae5eba5502b628f10e5df
Guillaume <guigiuruff@gmail.com>
2023-05-02 11:51:03 +0200

    added AcessPoint

--- lassab/TD1/main.py
+++ lassab/TD1/main.py
@@ -1,4 +1,13 @@
 from TD_package.FingerprintDatabase import FingerprintDatabase
+from TD_package.AccessPoint import AccessPoint
+
+
+
+AP = {"00:13:ce:95:e1:6f": AccessPoint("00:13:ce:95:e1:6f", 4.93, 25.81, 3.55, 2417000000, 5.0, 20.0), \
+      "00:13:ce:95:de:7e": AccessPoint("00:13:ce:95:de:7e", 4.83, 10.88, 3.78, 2417000000, 5.0, 20.0), \
+      "00:13:ce:97:78:79": AccessPoint("00:13:ce:97:78:79", 20.05, 28.31, 3.74, 2417000000, 5.0, 20.0), \
+      "00:13:ce:8f:77:43": AccessPoint("00:13:ce:8f:77:43", 4.13, 7.085, 0.80, 2417000000, 5.0, 20.0), \
+      "00:13:ce:8f:78:d9": AccessPoint("00:13:ce:8f:78:d9", 5.74, 30.35, 2.04, 2417000000, 5.0, 20.0)}
 
 db = FingerprintDatabase()
 db.read_csv('data.csv')
